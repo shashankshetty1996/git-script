de84d6ec55b19242e69ca6af435ec571f639ee77
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2018-08-18 12:58:00 +0530

    initial commit

---
+++ git.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# default push location
+REMOTE="origin"
+BRANCH="dev"
+
+# custom checking for origin and master
+if [ $# -eq "2" ]
+then
+  REMOTE="$1"
+  BRANCH="$2"
+elif [ $# -eq "1" ]
+then
+  REMOTE="$1"
+elif [ $# -gt "2" ]
+then
+  echo "Invalid usage of the script"
+  echo "usage: ./git.sh [origin] [branch]"
+  exit 1
+fi
+
+# Branch validaton
+if [ "$BRANCH" == "master" ]
+then
+  read -p "Are you sure you would select $BRANCH? (yes/no) " cond
+  if [ "$cond" != "yes" ]
+  then
+    echo "process has been terminated......."
+    echo "usage: ./git.sh [origin] [branch]"
+    exit 1
+  fi
+else
+  echo "not selected master branch"
+  git branch
+fi
+
+# Git commit message
+# read -p "Enter commit message here: " MSG
+
+# Git commands
+# git add .
+# git commit -m "$MSG"
+# git push $REMOTE $BRANCH
